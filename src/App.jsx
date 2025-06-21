4962ecac757633700c971d0368d777caefc612d4
JOSH-MOTION <joshuadoe168@gmail.com>
2025-06-21 14:27:30 +0000

    home

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar'
 import Home from './components/Home';
+import About from './components/about';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
       </Routes>
     </BrowserRouter>
   )
